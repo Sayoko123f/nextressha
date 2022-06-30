2caeacb4f58384161369de8a5e1c949757344f72
Sayoko123f <63059711+Sayoko123f@users.noreply.github.com>
2022-06-30 17:38:08 +0800

    add photoswipe

--- src/components/album-slide.vue
+++ src/components/album-slide.vue
@@ -30,26 +30,35 @@
             </router-link>
         </div>
         <div
+            id="pswp-lightbox"
             class="flex flex-col justify-center gap-0.5 px-px"
             :class="addClass.div"
         >
-            <img
-                class="max-w-full"
-                :class="addClass.img"
-                :src="`${serverImgBaseURL}/${item.key}/${page}`"
-                alt=""
+            <a
+                class="mx-auto block"
                 v-for="(page, key) in item.pages"
                 :key="key"
-                loading="lazy"
-                :id="`p${key}`"
-            />
+                :id="`a${key}`"
+                :href="`${serverImgBaseURL}/${item.key}/${page}`"
+            >
+                <img
+                    class="max-w-full"
+                    :class="addClass.img"
+                    :src="`${serverImgBaseURL}/${item.key}/${page}`"
+                    alt=""
+                    :id="`p${key}`"
+                    :onload="imgOnload"
+                />
+            </a>
         </div>
     </main>
 </template>
 <script setup>
 import { res, serverImgBaseURL } from '../assets/resource.json';
 import { useRoute } from 'vue-router';
-import { computed } from 'vue';
+import { computed, onMounted, onUnmounted } from 'vue';
+import PhotoSwipeLightbox from 'photoswipe/lightbox';
+import 'photoswipe/style.css';
 const route = useRoute();
 const id = route.params.id;
 const item = res[id];
@@ -58,5 +67,28 @@ const addClass = computed(() => {
         return { img: ['max-h-screen', 'object-contain'], div: [] };
     }
     return [{ img: [], div: [] }];
+});
+
+const lightbox = new PhotoSwipeLightbox({
+    gallery: '#pswp-lightbox',
+    children: 'a',
+    pswpModule: () => import('photoswipe'),
+});
+
+function imgOnload(event) {
+    const target = event.target;
+    const w = target.naturalWidth;
+    const h = target.naturalHeight;
+    const key = target.id.slice(1);
+    const a = document.querySelector(`#a${key}`);
+    a.setAttribute('data-pswp-width', w);
+    a.setAttribute('data-pswp-height', h);
+}
+
+onMounted(() => {
+    lightbox.init();
+});
+onUnmounted(() => {
+    lightbox.destroy();
 });
 </script>
